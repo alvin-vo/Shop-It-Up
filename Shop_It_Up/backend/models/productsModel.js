68dfe7bd395e0de18196aaf0b543a8734731f62f
Joshua Beed <jbeed33@gmail.com>
2024-02-05 20:35:17 -0800

    server now able to communicate with database

---
+++ Shop_It_Up/backend/models/productsModel.js
@@ -0,0 +1,18 @@
+const { Schema, mongoose } = require("mongoose");
+
+const productSchema = new Schema({
+  version: Number,
+  productId: String,
+  sellerId: String,
+  title: String,
+  quantity: Number,
+  description: String,
+  price: Number,
+  img: String,
+});
+
+productSchema.set("collection", "products");
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
