740f5b3a71e311291d433490abbb89a111f5c42b
Aidan Ching <achin053@ucr.edu>
2024-02-09 17:37:56 -0800

    chakra

--- Shop_It_Up/frontend/shop-it-up/src/features/HomePage/presentation/HomePage.tsx
+++ Shop_It_Up/frontend/shop-it-up/src/features/HomePage/presentation/HomePage.tsx
@@ -1,13 +1,11 @@
 import "./HomePage.css";
 
-
+import { Text } from '@chakra-ui/react'
 const HomePage = () => {
 
 
     return (
-        <div className = "home-page">
-            Home Page
-        </div>
+        <Text fontSize='6xl'>(6xl) In love with React & Next</Text>
 
     );
 }
