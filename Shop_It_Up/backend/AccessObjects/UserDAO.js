5007103b8e54c60ec9c7df981fc479c2181e324d
Joshua Beed <jbeed33@gmail.com>
2024-02-10 17:43:40 -0800

    added userDAO

---
+++ Shop_It_Up/backend/AccessObjects/UserDAO.js
@@ -0,0 +1,36 @@
+/* 
+These are the interfaces that communicate with the models. 
+The models communicate with the database.
+*/
+
+const Product = require("../models/UserModel.js");
+
+const createNewUser = async () => {};
+
+const udpateExistingUser = async () => {};
+
+const deleteExisitingUser = async () => {};
+
+const getExisitngUserInfo = async () => {};
+
+const addProduct = async () => {};
+
+const removeProduct = async () => {};
+
+const sendInvitation = async () => {};
+
+const acceptInvitation = async () => {};
+
+const receiveInvitation = async () => {};
+
+module.exports = {
+  createNewUser,
+  udpateExistingUser,
+  deleteExisitingUser,
+  getExisitngUserInfo,
+  addProduct,
+  removeProduct,
+  sendInvitation,
+  acceptInvitation,
+  receiveInvitation,
+};
