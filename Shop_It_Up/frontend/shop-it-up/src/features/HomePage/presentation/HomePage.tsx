797167d122418247f68c0ac4e8dcca92ea7edd17
Aidan Ching <achin053@ucr.edu>
2024-02-16 17:48:47 -0800

    login page

--- Shop_It_Up/frontend/shop-it-up/src/features/HomePage/presentation/HomePage.tsx
+++ Shop_It_Up/frontend/shop-it-up/src/features/HomePage/presentation/HomePage.tsx
@@ -1,10 +1,12 @@
 import "./HomePage.css";
 import ProductsGrid from '../../Products/Presentation/ProductsGrid';
 import Navbar from '../../NavBar/Presentation/Navbar'
+import LoginPage from "../../LoginPage/presentation/LoginPage";
 
 const HomePage = () => {
     return (
         <div>
+            <LoginPage/>
             <Navbar/>
                 <ProductsGrid/>
         </div>
