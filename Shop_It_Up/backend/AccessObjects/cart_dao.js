8bc04bbefb08662ae6329072152af8a88697ab83
Joshua Beed <jbeed33@gmail.com>
2024-02-18 14:03:38 -0800

    added cart dao

---
+++ Shop_It_Up/backend/AccessObjects/cart_dao.js
@@ -0,0 +1,29 @@
+/* 
+These are the interfaces that communicate with the models. 
+The models communicate with the database.
+*/
+
+const cartDAO = require("../models/cart_model,js");
+
+const addProduct = async (productId) => {};
+
+const removeProduct = async (productId) => {};
+
+const addContributor = async (cartId) => {};
+
+const removeContributor = async (userId) => {};
+
+const checkout = async (cartId, userId) => {};
+
+//I can't remeber why we have this
+const syncCart = async (userId) => {};
+
+const deleteCart = async (cartId) => {};
+module.exports = {
+  addProduct,
+  removeProduct,
+  removeContributor,
+  addContributor,
+  checkout,
+  syncCart,
+};
