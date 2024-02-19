2890bdd84521e2910cec4db0fdf1fd0cef40f28f
clam-emma <elam059@ucr.edu>
2024-02-19 12:38:37 -0800

    nav-bar

--- Shop_It_Up/frontend/shop-it-up/src/features/HomePage/presentation/HomePage.tsx
+++ Shop_It_Up/frontend/shop-it-up/src/features/HomePage/presentation/HomePage.tsx
@@ -1,9 +1,11 @@
 import "./HomePage.css";
 import ProductsGrid from '../../Products/Presentation/ProductsGrid';
+import NavBar from "../../NavBar/Presentation/NavBar";
 
 const HomePage = () => {
     return (
-        <ProductsGrid/>
+        // <ProductsGrid/>
+        <NavBar/>
 
     );
 }
