5ea95d18f76358f0a25a0d0392c1c765234cb667
etnard <drantelme@gmail.com>
2024-02-20 11:43:23 -0800

    Clean Up

--- Shop_It_Up/backend/Managers/cart_manager.js
+++ Shop_It_Up/backend/Managers/cart_manager.js
@@ -1,12 +1,26 @@
-const cartDAO = require("../models/cart_model.js");
+const cartDAO = require("../models/cart_dao.js");
 
-const addProductToCart = async (productId) => {};
+// PRODUCT UPDATE:
+
+// 0: CHECK IF CART IS EMPTY
+// 1: IF CART IS EMPTY -> 2, ELSE -> 3
+// 2: CREATE CART 
+// 3: ADD PRODUCT TO CART
+// 4: RETURN NEW CART
+const addProductToCart = async (cartId) => {
+  
+};
 
 const removeProductFromCart = async (productId) => {};
+
+
+// USER UPDATE:
 
 const addContributorToCart = async (cartId) => {};
 
 const removeContributorFromCart = async (userId) => {};
+
+// CART UPDATE
 
 const checkoutCart = async (cartId, userId) => {};
 
--- Shop_It_Up/backend/routes/cart_route.js
+++ Shop_It_Up/backend/routes/cart_route.js
@@ -7,21 +7,85 @@ const express = require("express");
 const router = express.Router();
 const cartManager = require("../Managers/cart_manager.js");
 
-//adding product to cart
-router.post("/addProduct", async (req, res) => {});
+// PRODUCT UPDATE:
 
-//removing cart
-router.delete("/:cartid", async (req, res) => {});
+// ADD TO CART: THIS SHOULD ADD TO CART AND RETURN THE NEW CART
+router.post("/addProduct/:cartId/:productId", async (req, res) => {
+  const cart = await cartManager.addProductToCart(req);
+  if (cart == null) {
+    // null or empty ?
+    res.send("Error: null.");
+  } else {
+    res.send(cart);
+  }
+});
 
-//removing contributor from cart
-router.delete("/:userId", async (req, res) => {});
+// ADD TO CART: THIS SHOULD REMOVE PRODUCT RELATED TO CART AND RETURN THE NEW CART
+router.post("/removeProduct/:cartId/:productId", async (req, res) => {
+  const cart = await cartManager.removeProductFromCart(req);
+  if (cart == null) {
+    // null or empty ?
+    res.send("Error: null.");
+  } else {
+    res.send(cart);
+  }
+});
 
-//adding contributor to cart
-router.post("/addContributor", async (req, res) => {});
+// USER UPDATE:
 
-//getting all products based on cart info
-router.get("/checkout/:cartId", async (req, res) => {
-  res.send("get all cart products");
+// REMOVE CONTRIBUTOR: SHOULD RETURN NEW CART WITH NEW CONTRIBUTORS
+router.post("/removeUser/:cartid/:userId", async (req, res) => {
+  const cart = await cartManager.removeContributorFromCart(req);
+  if (cart == null) {
+    // null or empty ?
+    res.send("Error: null.");
+  } else {
+    res.send(cart);
+  }
+});
+
+// ADD CONTRIBUTOR: SHOULD RETURN NEW CART WITH NEW CONTRIBUTORS
+router.post("/addUser/:cartid/:userId", async (req, res) => {
+  const cart = await cartManager.addContributorToCart(req);
+  if (cart == null) {
+    // null or empty ?
+    res.send("Error: null.");
+  } else {
+    res.send(cart);
+  }
+});
+
+// CART UPDATE:
+
+router.get("/", async (req, res) => {
+  const carts = await cartManager.getCarts(req);;
+  if (carts == null) {
+    res.send("Error: null.");
+  } else {
+    res.send(carts);
+  }
+});
+
+// CHECKOUT CART: SHOULD RETURN ALL PRODUCTS BASED ON USERID'S CART
+router.get("/checkout/:userId", async (req, res) => {
+  const cart = await cartManager.checkoutCart(req);
+  if (cart == null) {
+    // null or empty ?
+    res.send("Error: null.");
+  } else {
+    res.send(cart);
+  }
+});
+
+// REMOVE CART: SHOULD RETURN NULL
+router.delete("/removeCart/:cartid", async (req, res) => {
+  const cart = await cartManager.deleteCart(req);
+  if (cart == null) {
+    // null or empty ?
+    res.send("Error: null.");
+  } else {
+    res.send(cart);
+  }
 });
 
 module.exports = router;
