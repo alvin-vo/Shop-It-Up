d2e152f5d6fd9ba40717ac19305c919772b2f3bd
etnard <drantelme@gmail.com>
2024-02-20 12:17:58 -0800

    Almost Done: Add Product to Cart

--- Shop_It_Up/backend/AccessObjects/cart_dao.js
+++ Shop_It_Up/backend/AccessObjects/cart_dao.js
@@ -3,9 +3,12 @@ These are the interfaces that communicate with the models.
 The models communicate with the database.
 */
 
-const cartModel = require("../models/cart_model,js");
+const Cart = require("../models/cart_model,js");
 
-const addProduct = async (productId) => {};
+// ADD PRODUCT TO CART
+const addProduct = async (cartId, productId) => {
+
+};
 
 const removeProduct = async (productId) => {};
 
@@ -19,6 +22,43 @@ const checkout = async (cartId, userId) => {};
 const syncCart = async (userId) => {};
 
 const deleteCart = async (cartId) => {};
+
+const createCart = async(userIdForNewCart) => {
+  try {
+    createdCartId = makeNewId();
+    const newCart = new Cart({
+      cartId: createdCartId,
+      ownerId: userIdForNewCart,
+      contributorIds: [userIdForNewCart],
+      products: [],
+  })
+  newCart.save();
+    // Return created cartId.
+    return createdCartId;
+  } catch (err) {
+    return null; // Return null if error.
+  }
+};
+
+// HELPER FUNCTIONS
+
+// MAKE NEW ID (FOR CART) OF LENGTH 13
+function makeNewId() {
+  // SET VALUES CARTID CAN BE
+  const validChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-=!@#$%^&*()_+';
+  const validCharLength = validChar.length;
+  // VALUE TO RETURN
+  var returnVal = '';
+  // CREATE VALUE
+  let count = 0;
+  while (count < 13) {
+    returnVal += validChar.charAt(Math.floor(Math.random() * validCharLength));
+    count += 1;
+  }
+  // RETURN
+  return returnVal;
+}
+
 module.exports = {
   addProduct,
   removeProduct,
@@ -26,4 +66,6 @@ module.exports = {
   addContributor,
   checkout,
   syncCart,
+  deleteCart,
+  createCart,
 };
--- Shop_It_Up/backend/Managers/cart_manager.js
+++ Shop_It_Up/backend/Managers/cart_manager.js
@@ -1,14 +1,31 @@
 const cartDAO = require("../models/cart_dao.js");
+const userManager = require("../Managers/user_manager.js");
+const productManager = require("../Managers/product_manager.js");
 
 // PRODUCT UPDATE:
 
 // 0: CHECK IF CART IS EMPTY
+// 1: CHECK IF PRODUCT EXISTS
+// 2: RETURN IF PRODUCT DOESN'T EXIST
 // 1: IF CART IS EMPTY -> 2, ELSE -> 3
 // 2: CREATE CART 
 // 3: ADD PRODUCT TO CART
 // 4: RETURN NEW CART
-const addProductToCart = async (cartId) => {
-  
+const addProductToCart = async (passedInInfo) => {
+  const realCart = await userManager.checkValidCart(passedInInfo.params.cartId); // Should be TRUE
+  const realProduct = await productManager.getOneProduct(passedInInfo); // Should be set to a valid product
+
+  if(realProduct) { // REAL PRODUCT
+    if(realCart) { // REAL CART
+      return await cartDAO.addProduct(passedInInfo.params.cartId, realProduct.productId);
+    } else { // NEW CART
+      const userIdForCart = "707"; // TEMPORARY ID
+      const newCart = await createCart(userIdForCart); // PASS IN USER ID -> RETURNS NEW CART ID
+      return await cartDAO.addProduct(newCart, realProduct.productId);
+    }
+  }
+
+  return null;  // PRODUCT DOESN'T EXIST, ERROR
 };
 
 const removeProductFromCart = async (productId) => {};
@@ -25,6 +42,16 @@ const removeContributorFromCart = async (userId) => {};
 const checkoutCart = async (cartId, userId) => {};
 
 const deleteCart = async (cartId) => {};
+
+// HELPER FUNCTIONS
+
+async function createCart(passedInUserId) {
+  const newCartId = await cartDAO.createCart(passedInUserId); 
+  if(newCartId == null) {
+    return null;
+  }
+  return newCartId;
+};
 
 module.exports = {
   addProductToCart,
