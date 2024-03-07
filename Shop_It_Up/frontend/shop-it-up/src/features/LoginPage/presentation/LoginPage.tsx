bba1bec5385c4cc614ef7d899ada4078bab1009e
Shipapa1 <myduyisepic@gmail.com>
2024-03-06 17:34:34 -0800

    testing

--- Shop_It_Up/frontend/shop-it-up/src/features/LoginPage/presentation/LoginPage.tsx
+++ Shop_It_Up/frontend/shop-it-up/src/features/LoginPage/presentation/LoginPage.tsx
@@ -1,38 +1,31 @@
 import React, { useState } from 'react';
-import './LoginPage.css'; // Make sure the CSS file is correctly linked
-//import img1 from " ./images/shopitup.png";
-import { Box } from '@chakra-ui/react'
+import './LoginPage.css'; // Ensure the CSS file is correctly linked
+import { Box, Button } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
-
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const navigate = useNavigate();
 
-  
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Implement your login logic here
     console.log('Login attempt with email:', email, 'and password:', password);
     // Redirect or show error based on login logic
-    //history.push('/');
-    return //<div>Login Page</div>; //
   };
 
-  const navigateToHome = () => {
-    navigate('/'); // Use history.push to navigate to homepage
+
+
+  const navigateToGoogleAuth = () => {
+    window.location.href = "http://localhost:3010/api/authorize/auth/google"; // Redirects to Google Auth
   };
 
   return (
-
     <div className="login-page">
-
-
       <div className="login-container">
         <form onSubmit={handleSubmit} className="login-form">
-        <img src={" ./images/shopitup.png"} alt="Form Logo" style={{ width: '150px', margin: '0 auto', display: 'block' }} />
-          <h2>Login</h2>
+          <img src={" ./images/transparent-logo.png"} alt="Form Logo" style={{ width: '150px', margin: '0 auto', display: 'block' }} />
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input
@@ -54,8 +47,14 @@ const LoginPage: React.FC = () => {
             />
           </div>
           <button type="submit" className="login-button">Log In</button>
+          {/* Google Login Button */}
+          <Button onClick={navigateToGoogleAuth} colorScheme="red" size="md" className="google-login-button">
+            Google Login
+          </Button>
+          <Button  colorScheme="blue" size="md" className= "facebook-login-button" >
+            Facebook Login
+          </Button>
         </form>
-        <button onClick={navigateToHome} className="navigate-home-button">Go to Home</button> {/* New button for navigation */}
       </div>
     </div>
   );
